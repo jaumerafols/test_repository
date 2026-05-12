6313e3b2bbef8844ad7709c4165736e1754028f6
jaumerafols <calsebastianet@gmail.com>
2026-05-12 12:54:32 +0200

    Add files via upload

---
+++ exemple_streamlitf.py
@@ -0,0 +1,21 @@
+
+## pip install streamlit
+
+import streamlit as st
+
+st.title("My First App")
+st.write("Hello, world!")
+
+"""
+import pandas as pd
+
+# 1. Crear datos (diccionario de listas)
+data = {
+    'Nombre': ['Ana', 'Bob', 'Carlos', 'Diana'],
+    'Edad': [25, 30, 35, 28],
+    'Ciudad': ['Madrid', 'Bogotá', 'Lima', 'CDMX']
+}
+
+# 2. Crear el DataFrame
+df = pd.DataFrame(data)
+"""
